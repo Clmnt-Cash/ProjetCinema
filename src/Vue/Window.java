337084faf5f2a616713a4c8d14cbe86692b8570f
alexandreaaaaa <alexandre.gault88@gmail.com>
2024-04-07 19:17:35 +0200

    update de la classe Window avec nouvelle interface

--- src/Vue/Window.java
+++ src/Vue/Window.java
@@ -1,27 +1,44 @@
 package Vue;
 
-import javax.swing.JFrame;
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
 
     public Window() {
-        // Titre de la fenêtre
         super("Cinéma");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setSize(1500, 800);
 
+        //Obtenir les dimensions de l'écran
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int x = (screenSize.width - getWidth()) / 2;
         int y = (screenSize.height - getHeight()) / 2;
         setLocation(x, y);
 
+        JPanel drawComponents = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Color c1 = new Color(0, 0, 0);
+                g.setColor(c1);
+                g.fillRect(0, 100, 1500, 700);
+                Color c2 = new Color(50, 50, 50, 200);
+                g.setColor(c2);
+                g.fillRect(0, 0, 1500, 100);
+                ImageIcon logo = new ImageIcon("images/logos/logo_black.png");
+                Image image = logo.getImage();
+                g.drawImage(image, 0, 0, 100, 100, this);
+            }
+        };
+
+        add(drawComponents);
+
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        Window fenetre = new Window();
+        Window window = new Window();
     }
 }
