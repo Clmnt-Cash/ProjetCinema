30ae9fcdfda78623dfbab647bd5ad51421996dd0
oscar <oscar.aloisi@gmail.com>
2024-04-21 21:59:35 +0200

    Ajustement statistiques

--- src/Controleur/ControleurReduction.java
+++ src/Controleur/ControleurReduction.java
@@ -10,36 +10,38 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ControleurReduction {
-    private Client membre;
-    private GUIreduction vueReduction;
-    private Connexion connexion;
-    private ControleurEmployeAccueil controleurEmployeAccueil;
-    private GUIEmployeAccueil vueEmployeAccueil;
+    private Client membre; // Le client associé au contrôleur
+    private GUIreduction vueReduction; // Interface utilisateur pour les réductions
+    private Connexion connexion; // Connexion à la base de données
+    private ControleurEmployeAccueil controleurEmployeAccueil; // Contrôleur pour l'accueil des employés
+    private GUIEmployeAccueil vueEmployeAccueil; // Interface graphique pour l'accueil des employés
+
+    // Constructeur pour initialiser la connexion à la base de données
     public ControleurReduction(Connexion connexion) {
         this.connexion = connexion;
     }
 
+    // Configuration de l'interface utilisateur et ajout des écouteurs d'événements
     public void setVue(GUIreduction vue) {
         this.vueReduction = vue;
-        //Se déconnecter
+
+        // Écouteur pour le bouton de déconnexion
         MouseListener mouseListener = new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 JPanel panel = new JPanel();
                 panel.add(new JLabel("Etes-vous sûr de vouloir vous déconnecter ?"));
-
                 int resultat = JOptionPane.showConfirmDialog(null, panel, "Déconnexion", JOptionPane.OK_CANCEL_OPTION);
-
                 if (resultat == JOptionPane.OK_OPTION) {
                     vueReduction.closeWindow();
-                    GUIconnexion vueConnexion = new GUIconnexion();
-                    ControleurConnexion controleurConnexion = new ControleurConnexion(vueConnexion);
+                    new GUIconnexion();
+                    new ControleurConnexion(new GUIconnexion());
                 }
             }
         };
         this.vueReduction.addMouseListenerBoutonDeconnexion(mouseListener);
 
-        //Aller sur la page des films
+        // Écouteurs pour naviguer entre les différentes pages de l'application
         this.vueReduction.addListenerOngletFilms(e -> {
             vueReduction.closeWindow();
             controleurEmployeAccueil = new ControleurEmployeAccueil(connexion);
@@ -48,7 +50,7 @@ public class ControleurReduction {
             controleurEmployeAccueil.setVue(vueEmployeAccueil);
             controleurEmployeAccueil.openWindow();
         });
-        //Aller sur la page dex comptes
+
         this.vueReduction.addListenerOngletComptes(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -61,7 +63,6 @@ public class ControleurReduction {
             }
         });
 
-        //Aller sur la page des statistiques
         this.vueReduction.addListenerOngletStat(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -72,7 +73,8 @@ public class ControleurReduction {
                 controleurStatistiques.setMembre(membre);
             }
         });
-        //Modifier les réductions
+
+        // Enregistrement des nouvelles réductions
         this.vueReduction.addListenerEnregistrer(e -> {
             Reduction newReduc = vueReduction.getNewReduction();
             modifierReduction(newReduc.getReductionEnfant(), newReduc.getReductionRegulier(), newReduc.getReductionSenior());
@@ -83,33 +85,34 @@ public class ControleurReduction {
             controleurReduction.setMembre(membre);
         });
     }
+
+    // Assigner le membre client au contrôleur
     public void setMembre(Client membre) {
         this.membre = membre;
     }
 
+    // Méthode pour ouvrir la fenêtre de réduction
     public void openWindow(){
         this.vueReduction.setVisible(true);
     }
 
-    //Mathode pour modifier les réductions dans la bdd
+    // Modifier les réductions dans la base de données
     public void modifierReduction(int en, int r, int s){
         try {
-            String requeteInsertion = "UPDATE reduction SET enfant = " + en + ", regulier = " + r + ", senior = " + s ;
+            String requeteInsertion = "UPDATE reduction SET enfant = " + en + ", regulier = " + r + ", senior = " + s;
             connexion.executerRequete(requeteInsertion);
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    //Méthode pour récupérer les réductions
+    // Récupérer les réductions existantes
     public Reduction getReduction(){
         Reduction red = new Reduction(0, 0, 0);
         try {
             ArrayList<String> resultatReduc = connexion.remplirChampsRequete("SELECT * FROM reduction");
-
             for (String resultat : resultatReduc) {
                 String[] infosReduc = resultat.split(",");
-                //Extraire les informations sur le compte
                 int e = Integer.parseInt(infosReduc[0].trim());
                 int r = Integer.parseInt(infosReduc[1].trim());
                 int s = Integer.parseInt(infosReduc[2].trim());
